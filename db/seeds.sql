71704528f1b9dcade348eca9fc166ac86257bbd8
Mridula Udaya Shankar <mridu.sh92@gmail.com>
2018-06-07 09:52:46 -0700

    Create schema and seeds for Mysql

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO coffee(coffee_name) VALUES("Espresso");
+INSERT INTO coffee(coffee_name) VALUES("Café Latte");
+INSERT INTO coffee(coffee_name) VALUES("Cappuccino");
+INSERT INTO coffee(coffee_name) VALUES("Mocha");
+INSERT INTO coffee(coffee_name) VALUES("Macchiato");
