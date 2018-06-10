dd9a3290cf95f930b5e0551c0f7e83b1361c9395
Mridula Udaya Shankar <mridu.sh92@gmail.com>
2018-06-10 01:49:00 -0700

    Error fixes

--- controllers/coffeeshopControllers.js
+++ controllers/coffeeshopControllers.js
@@ -6,32 +6,28 @@ var router = express.Router();
 var coffees = require("../models/coffeeshop.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  coffees.all(function(result) {
+router.get("/", function (req, res) {
+  coffees.all(function (result) {
     res.render("index", result);
   });
 });
 
-router.post("/api/coffee", function(req, res) {
-  coffees.create(["name", "drink"], [req.body.name, req.body.drink], function(result) {
+router.post("/api/coffee", function (req, res) {
+  coffees.create(["name", "drink"], [req.body.name, req.body.drink], function (result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/coffee/:id/:drink", function(req, res) {
+router.put("/api/coffee/:id/:drink", function (req, res) {
   var condition = `id=${parseInt(req.params.id)}`;
-  var drink =  parseInt(req.params.drink);
-
-
-  console.log("condition", );
-
+  var drink = parseInt(req.params.drink);
   coffees.update(
     {
       drink
     },
     condition,
-    function(result) {
+    function (result) {
       if (result.changedRows === 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
